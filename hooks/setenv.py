6d7a70db0840cc3d55cd361fbde35448e4aebb0a
kiorky <devnull@localhost>
2008-05-24 12:56:58 +0200

    ready for minitage 0.4

--- hooks/setenv.py
+++ hooks/setenv.py
@@ -1,40 +1,40 @@
 import os
-import zc.buildout
-
-
-os_ldflags=''
-uname=os.uname()[0]
-if uname == 'Darwin':
-    os_ldflags=' -mmacosx-version-min=10.5.0'
-
-
-
-def appendEnvVar(env,var,sep=":",before=True):
-    """ append text to a environnement variable
-    @param env String variable to set
-    @param before append before or after the variable"""
-    for path in var:
-    	if before:os.environ[env] = "%s%s%s" % (path,sep,os.environ.get(env,''))
-	else:os.environ[env] = "%s%s%s" % (os.environ.get(env,''),sep,path)
-
+import re
 def getpsycopgenv(options,buildout):
-    for var in ['zlib','libiconv','openssl','postgresql']:
-        appendEnvVar('LDFLAGS', ["-L%(lib)s/lib -Wl,-rpath -Wl,%(lib)s/lib %(os)s"%{'lib':buildout[var]['location'],'os':os_ldflags}],sep=' ',before=False)
-        appendEnvVar('LD_RUN_PATH', ["%(lib)s/lib'"%{'lib':buildout[var]['location']}],sep=':',before=False)
-        appendEnvVar('CFLAGS', ["-I%s/include "%(buildout[var]['location'])],sep=' ',before=False)
-
-    appendEnvVar('CFLAGS', ["-I%s/include/openssl "%(buildout['openssl']['location'])],sep=' ',before=False)
-    os.environ['CPPFLAGS'] = os.environ['CFLAGS']
-    os.environ['CXXFLAGS'] = os.environ['CFLAGS']
     os.environ['LIBS'] = os.environ['LDFLAGS']
-    os.system("""
-SED=$(which gsed)
-if [[ ! -f $SED ]];then
-       SED=sed
-fi
-$SED -re "s:^(PY_MOD_DIR.*)$:PY_MOD_DIR=%s:g" -i Makefile.pre.in
-"""%options['site-packages'])
+def libxml(options, buildout, version):
+    """Patch Makefile to point to our site packages."""
+    pwd=options['compile-directory']
+    MAKEFILE = os.path.join(
+        pwd,
+        'Makefile'
+    )
+    file=open(MAKEFILE)
+    lines=file.readlines()
+    for i,line in enumerate(lines):
+        for word in 'PY_LIB_DIR', 'PY_MOD_DIR':
+            if line.startswith(word):
+                lines[i] = re.sub(
+                    '%s.*' % word,
+                    '%s = %s' % (
+                        word,
+                        os.path.join(
+                            buildout['buildout']['directory'],
+                            'parts',
+                            'site-packages-%s' % version)
+                    ),
+                    line
+                )
+    file = open(MAKEFILE,'w+')
+    file.writelines(lines)
+    file.close()
+    os.chdir(pwd)
 
+def make_24(options,buildout):
+    """Patch Makefile to point to our site packages."""
+    libxml(options, buildout, '2.4')
 
-
+def make_25(options,buildout):
+    """Patch Makefile to point to our site packages."""
+    libxml(options, buildout, '2.5')
 # vim:set ts=4 sts=4 et  :
